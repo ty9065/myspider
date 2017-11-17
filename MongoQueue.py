d19a44c91953d9e1263268885036873492823663
ty9065 <949137774@qq.com>
2017-11-17 12:19:40 +0800

    修改列表crawl_queue为数据库存储

---
+++ MongoQueue.py
@@ -0,0 +1,61 @@
+# -*- coding: UTF-8 -*-
+from datetime import datetime, timedelta
+from pymongo import MongoClient, errors
+
+class MongoQueue:
+
+    OUTSTANDING, PROCESSING, COMPLETE = range(3)
+
+    def __init__(self, client=None, timeout=300):
+        self.client = MongoClient() if client is None else client
+        self.db = self.client.cache
+        self.timeout = timeout
+
+    def __nonzero__(self):
+        record = self.db.crawl_queue.find_one({'status': {'$ne': self.COMPLETE}})
+        return True if record else False
+
+    def push(self, url):
+        try:
+            self.db.crawl_queue.insert({'_id': url, 'status': self.OUTSTANDING})
+        except errors.DuplicateKeyError as e:
+            pass
+
+    def pop(self):
+        record = self.db.crawl_queue.find_and_modify(
+            query={'status': self.OUTSTANDING},
+            update={'$set': {'status': self.PROCESSING,
+                             'timestamp': datetime.utcnow()}}
+        )
+
+        if record:
+            return record['_id']
+        else:
+            self.repair()                                                   # 没有待处理的url时，则检查处理中的url是否超时，超时则重新设为待处理
+            raise KeyError()
+
+    def peek(self):
+        record = self.db.crawl_queue.find_one({'status': self.OUTSTANDING})
+        if record:
+            return record['_id']
+
+    def complete(self, url):
+        self.db.crawl_queue.update({'_id': url},
+                                    {'$set': {'status': self.COMPLETE}})
+
+    def repair(self):
+                                                                            # find_and_modify：有返回值，modify可以是update，还可以是remove
+        record = self.db.crawl_queue.find_and_modify(
+            query={
+                'timestamp': {'$lt': datetime.utcnow() -
+                              timedelta(seconds=self.timeout)},
+                'status': {'$ne': self.COMPLETE}
+            },
+            update={'$set': {'status': self.OUTSTANDING}}
+        )                                                                   # '$lt'表示小于，'$ne'表示不相等
+
+        if record:
+            print 'Released:', record['_id']
+
+    def clear(self):
+        self.db.crawl_queue.drop()
